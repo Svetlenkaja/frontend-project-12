61036bdd2e474043a67b27759b50ba71f0e0b958
svetlenkaja <svetlenkajas@gmail.com>
2024-07-30 13:10:00 -0700

    label channelManagement

--- frontend/src/components/Channels.jsx
+++ frontend/src/components/Channels.jsx
@@ -91,7 +91,7 @@ const Channels = () => {
             <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z" />
             <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
           </svg>
-          <span className="visually-hidden">{t('titles.channelManagement')}</span>
+          <span className="visually-hidden">+</span>
         </button>
       </div>
       <ul id="channels-box" className="nav flex-column nav-pills nav-fill px-2 mb-3 overflow-auto h-100 d-block">
@@ -127,7 +127,9 @@ const Channels = () => {
                 <Dropdown.Toggle variant={`${
                   Number(currentChannel.id) === Number(channel.id) ? 'secondary' : ''
                 }`}
-                />
+                >
+                  <span className="visually-hidden">{t('titles.channelManagement')}</span>
+                </Dropdown.Toggle>
                 <Dropdown.Menu>
                   <Dropdown.Item onClick={() => activeModal(channel, 'remove')}>
                     {t('titles.menu.remove')}
