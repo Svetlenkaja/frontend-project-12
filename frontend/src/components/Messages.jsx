3c1dec2c7e5cd95b02f6f8e62424ce70a17721b2
svetlenkaja <svetlenkajas@gmail.com>
2024-08-04 12:20:23 -0700

    fix scroll

--- frontend/src/components/Messages.jsx
+++ frontend/src/components/Messages.jsx
@@ -13,7 +13,6 @@ const Messages = () => {
   const { t } = useTranslation();
   const { data: messages = [] } = useGetMessagesQuery();
   const { currentChannel } = useSelector((state) => state.app);
-  const scrollToRef = useRef(null);
   const chatBoxRef = useRef(null);
   const [isScrollTop, setIsScrollTop] = useState(false);
 
@@ -32,8 +31,9 @@ const Messages = () => {
     };
 
     chatBox.addEventListener('scroll', handleScroll);
+    console.log(isScrollTop);
     if (!isScrollTop) {
-      scrollToRef.current?.scrollIntoView({ behavior: 'smooth' });
+      chatBox.scrollTop = chatBox.scrollHeight;
     }
     return () => {
       chatBox.removeEventListener('scroll', handleScroll);
@@ -59,7 +59,6 @@ const Messages = () => {
               {message.body}
             </div>
           ))}
-          <div ref={scrollToRef} />
         </div>
         <div className="mt-auto px-5 py-3">
           <NewMessage />
